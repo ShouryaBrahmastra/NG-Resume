5423fd23b01c3b8d8a23605dceb2c4e3f7d39b34
Shourya Gupta <gptshourya02@gmail.com>
2022-01-06 16:29:37 +0530

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    /* When we click on each nav__link, we remove the show-menu class */
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -44,12 +44,12 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /* SHOW SCROLL TOP */ 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    /* When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class */
-    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollTop)
+window.addEventListener('scroll', scrollHeader)
 
 /* DARK LIGHT THEME */ 
 const themeButton = document.getElementById('theme-button')
@@ -101,7 +101,7 @@ let resumeButton = document.getElementById('resume-button')
 /* Html2pdf options */
 let opt = {
   margin:       0,
-  filename:     'Resume.pdf',
+  filename:     'Nishi Gupta.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
   jsPDF:        { format: 'a4', orientation: 'portrait' }
